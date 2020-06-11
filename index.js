b36db1c16878ea1c0caffd25c54c6e083c4fb84c
Jesse McMahon <Jessemcmahon17@gmail.com>
2020-06-11 15:15:42 -0400

    add modal

---
+++ index.js
@@ -0,0 +1,17 @@
+const button = document.querySelector(".btn");
+const modal = document.querySelector(".modal");
+const closeModal = document.getElementsByClassName("close")[0];
+
+button.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+});
